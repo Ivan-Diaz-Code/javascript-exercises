06a66d4a024f84dcf2a175effc05314fda5ac8d7
Ivan Diaz <zaid5224@gmail.com>
2025-03-26 19:20:26 -0700

    finished leapYears excersice

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(varNum) {
+    if(varNum % 4 === 0)
+    {
+        if(!(varNum % 100 === 0)){
+            return true;
 
+        }else if(varNum % 100 === 0 && varNum % 400 === 0){
+            return true;
+        }else {
+            return false;
+        }
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line

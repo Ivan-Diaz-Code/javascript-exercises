f0c391ca71f876b66f55dd98912966d21bfe3048
Ivan Diaz <zaid5224@gmail.com>
2025-03-26 19:08:40 -0700

    Finished sumAll excersice, some notes are that I had to look at solution to fix errors i had. But what i had was very similar to the actual solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(varOne, varTwo) {
+    if (!Number.isInteger(varOne) || !Number.isInteger(varTwo)) return "ERROR";
+    if (varOne < 0 || varTwo < 0) return "ERROR";
+    if(varOne > varTwo)
+    {
+        const temp = varOne;
+        varOne = varTwo;
+        varTwo = temp;
+    }
+
+    let sum = 0;
+    for (let i = varOne; i <= varTwo; i++) {
+        sum += i;
+    }
+    return sum;
+
+   
+
 
 };
 

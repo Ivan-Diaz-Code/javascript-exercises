eef047e65eabd50324075ddb2df6ae26025e71f6
Ivan Diaz <zaid5224@gmail.com>
2025-03-26 18:45:05 -0700

    Complted 7/8 tests with my method but could not complete one with multiple arguments since I did not know about the ...args for a paramter, had to look at solution to complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    /*
+   let i = 0;
+   while(i < arr.length)
+   {
+    if(arr[i] === varOne || arr[i] === varTwo)
+    {
+        arr.splice(i,1)
+    }else{
+        ++i;
+    }
+   }
+
+
+    return arr;
+    */
+   // create a new empty array
+  const newArray = [];
+  // use forEach to go through the array
+  array.forEach((item) => {
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  // and return that array
+  return newArray;
+
 };
 
 // Do not edit below this line
